56bb1fe932b39c5d568b2e818853ff1be5064459
Yamir Pinto Núñez <ypinto@geovictoria.com>
2023-03-16 16:54:35 -0300

    Formulario de creación de usuario
    Formulario de creación de usuario
    Signup apuntando a user#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :news
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_044208) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_181523) do
   create_table "news", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_044208) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
