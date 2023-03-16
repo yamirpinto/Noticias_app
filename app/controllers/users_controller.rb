884e2ff06bff08998a61d2f1986a8c86bf81c0db
Yamir Pinto Núñez <ypinto@geovictoria.com>
2023-03-16 17:52:13 -0300

    Edit Users
    Edit users funcional y sin errores.
    Form se adecua a new o edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,33 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Tu información de usuario ha sido correctamente actualizada"
+        redirect_to news_index_path
+      else
+        render 'edit'
+      end
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Bienvenido a Noticias Genosur #{@user.username}, Te has conectado exitosamente."
+        redirect_to news_index_path
+      else
+        render 'new'
+      end
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
